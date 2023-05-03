51814899bc12b7fcefc3e48406a9cc6607eb0e80
malek <malekhamdi290@gmail.com>
2023-05-03 22:29:12 +0100

    Look mom! A real app

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
-
 use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
@@ -33,8 +31,9 @@ class VinylController extends AbstractController
     public function browse(string $slug = null): Response
     {
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
         return $this->render('vinyl/browse.html.twig', [
-            'genre' => $genre,
+            'genre' => $genre
         ]);
     }
 }
